d6555d135bc1df450d5abb5db273dcbe9cdba710
GalvinPython <imgalvin@pm.me>
2026-02-08 11:55:06 +0000

    backing up before moving to mixins

--- src/main/java/me/imgalvin/Autolan.java
+++ src/main/java/me/imgalvin/Autolan.java
@@ -2,26 +2,29 @@ package me.imgalvin;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.server.integrated.IntegratedServer;
-import net.minecraft.text.Text;
-import net.minecraft.world.GameMode;
+import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.GameType;
 
-import static net.minecraft.util.NetworkUtils.findLocalPort;
+import static net.minecraft.util.HttpUtil.getAvailablePort;
 
 public class Autolan implements ClientModInitializer {
-	private boolean opened = false;
-
 	@Override
 	public void onInitializeClient() {
+		System.out.println("Autolan initialized!");
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			if (!opened && client.player != null && client.getServer() != null) {
-				IntegratedServer server = client.getServer();
+			System.out.println("Checking for player and server...");
+			if (client.player != null && client.getCurrentServer() != null) {
+				IntegratedServer server = client.getSingleplayerServer();
 
-				if (server.isSingleplayer()) {
-					int port = findLocalPort();
-					server.openToLan(GameMode.CREATIVE, true, port);
-					client.player.sendMessage(Text.of("§aOpened to LAN on port " + port), false);
-					opened = true;
+				System.out.println("Checking for LAN...");
+                if (server == null) return;
+
+				System.out.println("Opening LAN server");
+                if (server.isSingleplayer() && !server.isPublished()) {
+					int port = getAvailablePort();
+					server.publishServer(GameType.CREATIVE, true, port);
+					client.player.displayClientMessage(Component.literal("§aOpened to LAN on port " + port), false);
 				}
 			}
 		});
